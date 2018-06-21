945272da922020ddf1e0be9e7da57f981bb9be36
Your Name <hbtanjun2012@126.com>
2018-06-21 18:18:49 +0800

    优化一些内容

--- app/src/main/java/com/tan/mvpdemo/uitl/PreferencesUtil.java
+++ app/src/main/java/com/tan/mvpdemo/uitl/PreferencesUtil.java
@@ -50,12 +50,6 @@ public class PreferencesUtil {
     //	 *
     //	 * @return sp数据操作工具类
     //	 */
-    //	public static synchronized PreferencesUtil getSpUtil() {
-    //		if (spUtil == null) {
-    //			spUtil = new PreferencesUtil();
-    //		}
-    //		return spUtil;
-    //	}
 
     /**
      * 保存数据的方法，我们需要拿到保存数据的具体类型，然后根据类型调用不同的保存方法
